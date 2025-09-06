a7f703ba7998303c1d8c75628429c10e11ba50ae
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2025-09-06 15:03:09 +0200

    Minor fix

--- add_sources.php
+++ add_sources.php
@@ -5,6 +5,8 @@ require_once 'file_loader.php';
 // add an id "la" somewhere
 // php add_sources_debug.php DEBUG la
 
+define('DEBUG', 'debug');
+
 if( $argc < 3 ) {
   echo "Error: At least 2 arguments required: <base_constant> <id1> [id2] ...\n";
   exit(1);
@@ -14,8 +16,6 @@ if( ! defined( $argv[1] ) ) {
   echo "Error: First argument must be a defined constant (e.g., DEBUG)\n";
   exit(1);
 }
-
-define('DEBUG', 'debug');
 
 $cache = 'debug/cache.json';
 $base  = constant( $argv[1] );
